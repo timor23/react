0cf48cd89fcf22a1333a1e4ffc8e1833093e4aa2
timor23 <timors23@protonmail.com>
2021-10-25 15:34:00 +0300

    18.1

--- src/components/ex18/ex18_1/showText.js
+++ src/components/ex18/ex18_1/showText.js
@@ -1,20 +1,27 @@
 import React from "react";
-import TextInput from "./textInput";
 
 const ShowText = () => {
-    const [maxLength, setMaxLength] = React.useState(80);
+    const [maxLength, setMaxLength] = React.useState(30);
     const [text, setText] = React.useState("");
     const [showAll, setShowAll] = React.useState(false);
+    const [spanStyle, setSpanStyle] = React.useState(
+        {
+            color: 'blue',
+            cursor: 'pointer',
+            display: 'none'
+        }
+    )
 
     const handleText = (e) => {
         let str = e.target.value;
-        // if (str.length > maxLength) {
-        //     setText(str.slice(0, maxLength));
-        //     setShowAll(false);
-        // } else {
-        //     setText(e.target.value);
-        // }
         setText(str);
+        let tempObj = {...spanStyle};
+        if (str.length > maxLength) {
+            tempObj['display'] = 'block';
+        } else {
+            tempObj['display'] = 'none';
+        }
+        setSpanStyle(tempObj);
     }
 
     return (
@@ -22,7 +29,8 @@ const ShowText = () => {
             <textarea name="text" id="txt" cols="30" rows="10" onChange={handleText}></textarea>
             <div style={{inlineSize: '400px', overflowWrap: 'break-word'}}>
                 {showAll ? text : text.substr(0, maxLength)}
-                <span style={{color: 'blue', cursor: 'pointer'}} onClick={() => setShowAll(!showAll)}>{showAll ? '...show less' : '...show more'}</span>
+                <span style={spanStyle}
+                      onClick={() => setShowAll(!showAll)}>{showAll ? '...show less' : '...show more'}</span>
             </div>
         </div>
     )
