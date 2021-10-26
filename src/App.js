bde502636845f733d2bd12dfddd4007cdf0d580c
timor23 <timors23@protonmail.com>
2021-10-27 02:47:47 +0300

    19.1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import ShowText from './components/ex18/ex18_1/showText';
 import Todo from "./components/ex18/ex18_2/todo";
 import MarkAndDel from "./components/ex18/18_3/markAndDel";
-import SimpleFetch from './components/ex19/ex19_1'
+import SimpleFetch from './components/ex19/ex19_1/ex19_1'
 
 function App() {
     return (
